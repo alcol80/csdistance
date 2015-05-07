c86b5e201c44f0e9a4d3f21b7249ab84d4619300
Alessio Coletta <alcol80@gmail.com>
2015-05-07 23:34:42 +0200

    Mods for caching

--- scripts/install_z3prover.sh
+++ scripts/install_z3prover.sh
@@ -2,14 +2,18 @@
 
 basedir=$(pwd)
 targetdir="$basedir/local"
-mkdir -p "$targetdir"
 
-echo Cloning z3: branch $Z3_BRANCH
-git clone --depth 1 --branch "$Z3_BRANCH" https://github.com/Z3Prover/z3.git
+if [ ! -d "$targetdir" ]; then
+  mkdir -p "$targetdir"
 
-echo Build and install z3 into directory $targetdir
-cd z3
-python scripts/mk_make.py --prefix="$targetdir"
-cd build
-make
-make install
+  echo Cloning z3: branch $Z3_BRANCH
+  git clone --depth 1 --branch "$Z3_BRANCH" https://github.com/Z3Prover/z3.git
+
+  echo Build and install z3 into directory $targetdir
+  cd z3
+  git branch
+  python scripts/mk_make.py --prefix="$targetdir"
+  cd build
+  make
+  make install
+fi
