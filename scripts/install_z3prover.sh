b0b40ad8607e5ce3c3cb9d43e89f0d346b69b556
Alessio Coletta <alcol80@gmail.com>
2015-05-07 18:21:02 +0200

    Added: z3 build and install

---
+++ scripts/install_z3prover.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+basedir=$(pwd)
+targetdir="$basedir/local"
+mkdir -p "$targetdir"
+
+echo Cloning z3: branch $Z3_BRANCH
+git clone --depth 1 --branch "$Z3_BRANCH" https://github.com/Z3Prover/z3.git
+
+echo Build and install z3 into directory $targetdir
+cd z3
+python scripts/mk_make.py --prefix="$targetdir"
+cd build
+make
+make install
