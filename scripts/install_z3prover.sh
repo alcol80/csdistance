1ad101f5b9773313cf11aec56b4603b33cd5414e
Alessio Coletta <alcol80@gmail.com>
2015-05-23 19:29:30 +0200

    little mod to z3 installer script (debug)

--- scripts/install_z3prover.sh
+++ scripts/install_z3prover.sh
@@ -4,16 +4,19 @@ basedir=$(pwd)
 targetdir="$basedir/local"
 
 if [ ! -d "$targetdir" ]; then
-  mkdir -p "$targetdir"
+    mkdir -p "$targetdir"
 
-  echo Cloning z3: branch $Z3_BRANCH
-  git clone --depth 1 --branch "$Z3_BRANCH" https://github.com/Z3Prover/z3.git
+    echo Cloning z3: branch $Z3_BRANCH
+    git clone --depth 1 --branch "$Z3_BRANCH" https://github.com/Z3Prover/z3.git
 
-  echo Build and install z3 into directory $targetdir
-  cd z3
-  git branch
-  python scripts/mk_make.py --prefix="$targetdir"
-  cd build
-  make
-  make install
+    echo Build and install z3 into directory $targetdir
+    cd z3
+    git branch
+    python scripts/mk_make.py --prefix="$targetdir"
+    cd build
+    make
+    make install
+else
+    echo "[DEBUG] Directory $targetdir exists:"
+    ls -laR "$targetdir"
 fi
